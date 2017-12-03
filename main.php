3da2f28c450ff8976f791868c6729be04f5d99bf
PrebenHan <34210932+PrebenHan@users.noreply.github.com>
2017-12-03 15:35:59 +0100

    Create main.php

---
+++ main.php
@@ -0,0 +1,19 @@
+<?php
+include_once("utility.php");
+// prepare db
+$db = new Database();
+$db->wipeClean();
+// prepare doc
+$doc = new DOMDocument();
+if (!$doc->load('SkierLogs.xml')) {
+    p("COULDN'T LOAD XML");
+} else {
+    // do job
+    $doc->normalize();
+    $info = parse($doc); 
+    $info->unify();
+    $info->print();
+    putInto($info, $db);
+    p("success!");
+}
+?>
